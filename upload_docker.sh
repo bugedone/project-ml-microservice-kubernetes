56c5632f2f288638c7a367af23c62b59ba685c77
Stu <stu@stu.nu>
2023-09-07 09:29:00 +0100

    Initialising repo

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=bugedone/project-ml-microservice:v1.0.0
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag project-ml-microservice $dockerpath
+docker images
+docker login
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
